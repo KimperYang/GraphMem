7a95e3d77b389d75b0d0681ad76e33a423108512
yaoguany <89233842+yaoguany@users.noreply.github.com>
2024-12-08 18:42:45 -0800

    yaogy update

--- src/graph/knowledge_graph.py
+++ src/graph/knowledge_graph.py
@@ -5,73 +5,108 @@ class SemanticKnowledgeGraph:
     def __init__(self, model_name='all-MiniLM-L6-v2'):
         self.graph = nx.DiGraph()
         self.model = SentenceTransformer(model_name)
-    
+
     def add_node(self, node):
         if node not in self.graph:
-            self.graph.add_node(node)
-    
+            embedding = self.model.encode(node)
+            self.graph.add_node(node, embedding=embedding)
+
     def add_edge(self, node1, node2, relation, overwrite=False):
         self.add_node(node1)
         self.add_node(node2)
-        
-        new_relation_emb = self.model.encode(relation)
-        
+
+        relation_embedding = self.model.encode(relation)
+
         if self.graph.has_edge(node1, node2):
             existing_relation = self.graph[node1][node2].get('relation', None)
-            if existing_relation == relation:
+            if overwrite:
+                self.graph[node1][node2]['relation'] = relation
+                self.graph[node1][node2]['relation_embedding'] = relation_embedding
                 return {
                     'conflict': True,
-                    'message': f"Edge ({node1}, {node2}) with relation '{relation}' already exists",
+                    'message': f"Edge ({node1}, {node2}) relation updated to '{relation}'",
                 }
-                
-            else:
-                self.graph.add_edge(node1, node2, relation=relation, relation_embedding=new_relation_emb)
-                return {
-                    'conflict': False,
-                    'message': f"Edge ({node1}, {node2}) with relation '{relation}' added",
-                }
+
+            return {
+                'conflict': True,
+                'message': f"Edge ({node1}, {node2}) already has relation '{existing_relation}'",
+            }
+
         else:
-            self.graph.add_edge(node1, node2, relation=relation, relation_embedding=new_relation_emb)
+            self.graph.add_edge(node1, node2, relation=relation, relation_embedding=relation_embedding)
             return {
                 'updated': False,
-                'message': f"Edge ({node1}, {node2}) with relation '{relation}' added",
+                'message': f"Added edge ({node1}, {node2}) with relation '{relation}'",
             }
 
     def query(self, node1=None, node2=None, relation=None, top_k=1):
-        query_node1_emb = self.model.encode(node1) if node1 else None
-        query_node2_emb = self.model.encode(node2) if node2 else None
-        query_relation_emb = self.model.encode(relation) if relation else None
+        assert (node1 is None) + (node2 is None) + (relation is None) == 1, "Only one element of the triplet can be None during query"
 
         matches = []
         scores = []
 
-        for n1, n2, attrs in self.graph.edges(data=True):
-            score = 0
+        if node1 is None:
+            # Given node2 and relation, find node1
+            query_node2_emb = self.graph.nodes[node2]['embedding']
+            query_relation_emb = self.model.encode(relation)
+            for n1 in self.graph.predecessors(node2):
+                n1_emb = self.graph.nodes[n1]['embedding']
+                rel_emb = self.graph[n1][node2]['relation_embedding']
 
-            # Compare node1
-            if query_node1_emb is not None:
-                score += util.cos_sim(query_node1_emb, self.graph.nodes[n1]['embedding']).item()
+                node_score = util.cos_sim(n1_emb, query_node2_emb).item()
+                rel_score = util.cos_sim(rel_emb, query_relation_emb).item()
+                total_score = node_score + rel_score
 
-            # Compare node2
-            if query_node2_emb is not None:
-                score += util.cos_sim(query_node2_emb, self.graph.nodes[n2]['embedding']).item()
+                matches.append(n1)
+                scores.append(total_score)
 
-            # Compare relation
-            if query_relation_emb is not None:
-                score += util.cos_sim(query_relation_emb, attrs['relation_embedding']).item()
+            top_indices = sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)[:top_k]
+            top_k_nodes = [(matches[i], scores[i]) for i in top_indices]
+            return top_k_nodes
 
-            matches.append((n1, n2))
-            scores.append(score)
+        elif node2 is None:
+            # Given node1 and relation, find node2
+            query_node1_emb = self.graph.nodes[node1]['embedding']
+            query_relation_emb = self.model.encode(relation)
+            for n2 in self.graph.successors(node1):
+                n2_emb = self.graph.nodes[n2]['embedding']
+                rel_emb = self.graph[node1][n2]['relation_embedding']
 
-        top_k_matches = [matches[i] for i in sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)[:top_k]]
-        top_k_scores = [scores[i] for i in sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)[:top_k]]
-        
-        return top_k_matches, top_k_scores
+                node_score = util.cos_sim(n2_emb, query_node1_emb).item()
+                rel_score = util.cos_sim(rel_emb, query_relation_emb).item()
+                total_score = node_score + rel_score
 
-if __name__ == "__main__":   
+                matches.append(n2)
+                scores.append(total_score)
+
+            top_indices = sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)[:top_k]
+            top_k_nodes = [(matches[i], scores[i]) for i in top_indices]
+            return top_k_nodes
+
+        else:
+            # Given node1 and node2, find relation
+            if self.graph.has_edge(node1, node2):
+                rel = self.graph[node1][node2]['relation']
+                total_score = 2.0
+                return [(rel, total_score)]
+            else:
+                return []
+
+if __name__ == "__main__":
     kg = SemanticKnowledgeGraph()
+    kg.add_edge("A", "B", "cooperate")
+    kg.add_edge("B", "C", "compete")
+    kg.add_edge("A", "C", "friend")
+    kg.add_edge("A", "D", "enemy")
 
-    kg.add_edge("A", "B", "works with")
-    kg.add_edge("A", "B", "collaborates alongside")
-    kg.add_edge("A", "B", "is a competitor of")
-    print("Current relation between A and B:", kg.query_edge("A", "B"))
+    # Query the relation between A and B
+    relations = kg.query(node1="A", node2="B", relation=None)
+    print("Relation between A and B:", relations)
+
+    # Given node A and relation "cooperate", find nodes
+    nodes = kg.query(node1="A", node2=None, relation="friend", top_k=2)
+    print("Nodes that cooperate with A:", nodes)
+
+    # Given node C and relation "compete", find starting nodes
+    nodes = kg.query(node1=None, node2="C", relation="compete")
+    print("Nodes that compete with C:", nodes)
